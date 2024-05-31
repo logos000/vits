3654248ca5e5b4e3829030639f6ceeda032e8241
logos000 <shunyang.22@intl.zju.edu.cn>
2024-05-31 11:26:43 +0800

    vits

---
+++ main.py
@@ -0,0 +1,111 @@
+import os
+import yaml
+from mirai import *
+
+from pkg.plugin.context import register, handler, BasePlugin, APIHost, EventContext
+from pkg.plugin.events import *  # 导入事件类
+from pkg.command import entities
+from pkg.command.operator import CommandOperator, operator_class
+import typing
+
+from plugins.vits.pkg.generate_voice import generate_audio
+
+# 读取yaml配置文件
+with open(os.path.join(os.path.dirname(__file__), 'config.yml'), 'r', encoding='utf-8') as f:
+    config = yaml.safe_load(f)
+
+
+ifVoice = config['voice_switch']
+language = config['language']
+
+
+# 增加指令
+@operator_class(name="vits", help="vits开关 on/off/status", privilege=1)
+class SwitchVoicePlugin(CommandOperator):
+
+    # 控制语音合成的开关
+    async def execute(self, context: entities.ExecuteContext) -> typing.AsyncGenerator[entities.CommandReturn, None]:
+        global ifVoice
+        global language
+
+        if context.crt_params[0] == "on":
+            self.ap.logger.info("启动语音合成")
+            ifVoice = True
+            yield entities.CommandReturn(text="启动语音合成")
+
+        elif context.crt_params[0] == "off":
+            self.ap.logger.info("关闭语音合成")
+            ifVoice = False
+            yield entities.CommandReturn(text="关闭语音合成")
+
+        elif context.crt_params[0] == "status":
+            if ifVoice:
+                yield entities.CommandReturn(text="语音合成状态:已开启" + f"，当前语言为{language}")
+            else:
+                yield entities.CommandReturn(text="语音合成状态:已关闭")
+
+        elif context.crt_params[0] == "language":
+
+            reply = "语言列表：\n 中文  英文    日文"
+                    
+
+            yield entities.CommandReturn(text=reply.strip())
+
+        elif context.crt_params[0] == "switch":
+           
+           language = context.crt_params[1]
+           self.ap.logger.info(f"切换语音合成语言为{language}")
+           yield entities.CommandReturn(text=f"切换语音合成语言为{language}")
+
+        elif context.crt_params[0] == "帮助":
+            yield entities.CommandReturn(text="vits插件支持的指令有：\n"
+                                              "!vits on\n"
+                                              "!vits off\n"
+                                              "!vits status\n"
+                                              "!vits language\n"
+                                              "!vits switch\n"
+                                              "!vits help\n")
+
+        else:
+            yield entities.CommandReturn(error="无效指令，请输入\"!vits help\"查看帮助")
+
+
+# 注册插件
+@register(name="vits", description="一个语音插件", version=".1", author="logos")
+class VoicePlugin(BasePlugin):
+    # 插件加载时触发
+    def __init__(self, host: APIHost):
+        # 清空同目录的audio_temp文件夹下的所有文件
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "audio_temp"))
+        
+        if os.path.exists(base_path):
+            for file in os.listdir(base_path):
+                os.remove(os.path.join(base_path, file))
+        pass
+
+    # 当消息回复时触发
+    @handler(NormalMessageResponded)
+    async def text_to_voice(self, ctx: EventContext):
+        #print(1111111111111111)
+        global ifVoice
+        global language
+        # 如果语音开关开启
+        if ifVoice:
+            # 获取回复内容
+            res_text = ctx.event.response_text
+
+            self.ap.logger.info(f"使用角色生成回复语音")
+            # 生成语音
+            result = generate_audio(res_text, language)
+            
+            if result:
+                # 回复语音消息
+                ctx.add_return("reply", [Voice(path=str(result))])
+
+                # 删除生成的silk语音文件
+                #os.remove(result)
+            #print(121,result)
+            
+    def __init__(self, host: APIHost):
+        pass
+
---
+++ pkg/generate_voice.py
@@ -0,0 +1,74 @@
+import requests
+import json
+import os
+import yaml
+import time
+from pathlib import Path
+from graiax import silkcoder
+
+# 设置基础路径为当前文件夹的上一级
+base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+
+
+
+def generate_audio(text, language):
+    url_ref = "http://127.0.0.1:9880"
+    data_ref = {"text":text, "text_language": language}
+
+    response = requests.post(url = url_ref, json=data_ref)
+
+    #if response:
+    # 设置基础路径为当前文件夹的上一级
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    # 在基础路径下创建audio_temp文件夹
+    base_path = os.path.join(base_path, "audio_temp")
+    #print(base_path)
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+    # 提取文件名，.wav字符前的八个字符为文件名
+    #file_name = audio_url.split('/')[-1].split('.')[0][:8]
+    # 拼接文件路径
+    save_path = os.path.join(base_path, 'output.wav')
+    #if not os.path.exists(save_path):
+    #    os.makedirs(save_path)
+    #print(save_path)
+        # 检查请求是否成功
+    if response.status_code == 200:
+        # 将响应内容写入文件
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+        print("音频文件已成功保存为 'output.wav'")
+    else:
+        print(f"请求失败，状态码: {response.status_code}")
+        #if download_audio(audio_url, save_path):
+           
+            # 转换为silk格式
+    silk_path = convert_to_silk(save_path, base_path)
+
+
+
+            # 删除wav文件
+    #os.remove(save_path)
+    return silk_path
+ 
+
+    # 转换为silk
+def convert_to_silk(wav_path: str, temp_folder: str) -> str:
+    silk_path = os.path.join(temp_folder, Path(wav_path).stem + '.silk')
+    if os.path.exists(silk_path):
+        os.remove(silk_path)
+        time.sleep(0.1)
+    silkcoder.encode(wav_path, silk_path)
+
+    # print(f"已将 WAV 文件 {wav_path} 转换为 SILK 文件 {silk_path}")
+    return silk_path
+
+
+if __name__ == "__main__":
+    # get_character_list()
+
+    # 测试生成语音
+    text = "你好"
+    #character = "430"
+    generate_audio(text)
